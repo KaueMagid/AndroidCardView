3178f3158e2c448f829809099abeaf46e732b5f5
Kaue Salem <magidsalem96@gmail.com>
2021-02-18 16:25:35 -0300

    config adapter

--- src/main/java/com/kauemagid/cardview/adapters/PostsAdapter.java
+++ src/main/java/com/kauemagid/cardview/adapters/PostsAdapter.java
@@ -1,6 +1,7 @@
 package com.kauemagid.cardview.adapters;
 
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,23 +10,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kauemagid.cardview.R;
+
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder> {
 
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemList = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.post_layout, parent, false);
+        return new MyViewHolder(itemList);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.username.setText("Kaue Salem");
+        holder.description.setText("Muito bom o pavê");
+        holder.imageView.setImageResource(R.drawable.imagem2);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
@@ -36,6 +43,11 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            username = itemView.findViewById(R.id.textView_username);
+            description = itemView.findViewById(R.id.textView_description);
+            imageView = itemView.findViewById(R.id.imagePost);
+
+
         }
     }
 
